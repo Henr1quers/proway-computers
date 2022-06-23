5be6cc2232cb6c829178ff36c1afa280b8ecbdaa
Henrique <henrique-rodrigues10@hotmail.com>
2022-06-23 09:54:20 -0300

    build

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'teste'`, () => {
+  it(`should have as title 'Projeto-Angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('teste');
+    expect(app.title).toEqual('Projeto-Angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('teste app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Projeto-Angular app is running!');
   });
 });
